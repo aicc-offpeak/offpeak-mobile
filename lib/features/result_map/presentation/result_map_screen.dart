fec934ac93efc42f8c0e2736bf906934fadecac5
altoel <alto.ellya@gmail.com>
2026-01-08 17:27:27 +0900

    chore: 업데이트된 README 및 Android 권한 추가, ResultMapScreen 리팩토링

--- lib/features/result_map/presentation/result_map_screen.dart
+++ lib/features/result_map/presentation/result_map_screen.dart
@@ -1,57 +1,15 @@
 import 'package:flutter/material.dart';
 
-import '../../../shared/widgets/error_view.dart';
-import '../../../shared/widgets/loading.dart';
-import '../state/result_map_controller.dart';
 import '../widgets/map_view.dart';
-import '../widgets/place_list_sheet.dart';
 
-class ResultMapScreen extends StatefulWidget {
+/// Android 전용: 지도만 전체 화면으로 표시하는 베이스라인 화면.
+class ResultMapScreen extends StatelessWidget {
   const ResultMapScreen({super.key});
 
   @override
-  State<ResultMapScreen> createState() => _ResultMapScreenState();
-}
-
-class _ResultMapScreenState extends State<ResultMapScreen> {
-  final controller = ResultMapController();
-
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: controller,
-      builder: (context, _) {
-        return Stack(
-          children: [
-            const MapView(),
-            if (controller.isLoading)
-              const Positioned.fill(child: IgnorePointer(child: LoadingView())),
-            if (controller.error != null)
-              Positioned(
-                top: 48,
-                left: 16,
-                right: 16,
-                child: ErrorView(
-                  message: controller.error!,
-                  onRetry: () => controller.searchAround(''),
-                ),
-              ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: PlaceListSheet(
-                places: controller.places,
-                onSelect: controller.focus,
-              ),
-            ),
-          ],
-        );
-      },
+    return const Scaffold(
+      body: MapView(),
     );
   }
 }
--- lib/features/result_map/widgets/map_view.dart
+++ lib/features/result_map/widgets/map_view.dart
@@ -1,8 +1,9 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:kakao_map_sdk/kakao_map_sdk.dart';
 
 /// Android 전용 KakaoMap MVP 베이스라인.
-/// Poi 기반으로 지도 표시.
+/// POI 기반으로 지도 표시. labelLayer.addPoi 사용.
 class MapView extends StatefulWidget {
   const MapView({super.key});
 
@@ -14,29 +15,60 @@ class _MapViewState extends State<MapView> {
   KakaoMapController? _controller;
   bool _isMapReady = false;
 
-  // 하드코딩된 더미 좌표 (서울 강남역)
-  static const _dummyLat = 37.4979;
-  static const _dummyLng = 127.0276;
+  // 하드코딩된 좌표 및 POI 텍스트
+  static const _lat = 37.4810;
+  static const _lng = 126.8826;
+  static const _zoomLevel = 16;
+  static const _poiText = "테스트 마커";
 
   @override
   Widget build(BuildContext context) {
     return KakaoMap(
       option: const KakaoMapOption(
-        position: LatLng(_dummyLat, _dummyLng),
-        zoomLevel: 16,
+        position: LatLng(_lat, _lng),
+        zoomLevel: _zoomLevel,
       ),
       onMapReady: (controller) {
+        debugPrint('[MapView] onMapReady called - 지도 로딩 완료');
+        
         setState(() {
           _controller = controller;
           _isMapReady = true;
         });
+
+        // 지도 준비 완료 후 POI 추가
+        _addPoi();
+        
       },
     );
+  }
+
+  void _addPoi() {
+    if (!_isMapReady || _controller == null) {
+      debugPrint('[MapView] POI 추가 실패: 지도가 아직 준비되지 않음');
+      return;
+    }
+
+    debugPrint('[MapView] POI 추가 시도: ($_lat, $_lng) - "$_poiText"');
+
+    // labelLayer.addPoi로 텍스트 기반 POI 추가
+    // addPoi는 LatLng를 첫 번째 인자로 받고, text와 style을 named parameter로 받음
+    try {
+      _controller!.labelLayer.addPoi(
+        const LatLng(_lat, _lng),
+        text: _poiText,
+        style: PoiStyle(),
+      );
+      debugPrint('[MapView] POI 추가 성공');
+    } catch (e) {
+      debugPrint('[MapView] POI 추가 실패: $e');
+    }
   }
 
   @override
   void dispose() {
     _controller = null;
+    _isMapReady = false;
     super.dispose();
   }
 }
